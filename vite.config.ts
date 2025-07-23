a0c878fff4427bbd6d121c3035dd880ee601e1f7
IsaacHuo <2210286979@qq.com>
2025-07-23 15:32:02 +0800

    path_fix

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/qurelab-website/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/qurelab/' : '/',
   plugins: [
     vue(),
     vueJsx(),
